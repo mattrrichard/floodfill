35aa63fb923d8dac76e70eb7a1f37925100f4508
Matt Richard <matt@richardsoftware.com>
2016-06-13 01:09:35 -0500

    Randomly generate the cell colors on startup

--- src/Main.elm
+++ src/Main.elm
@@ -1,13 +1,66 @@
 module Main exposing (..)
 
+import Array
 import Board
 import Cell
+import Color exposing (Color)
 import Html exposing (..)
 import Html.App as Html
 import Html.Events as Html
+import Random
+import Random.Array
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Svg.Events as Svg
+
+
+type alias Config =
+    { rows : Int
+    , cols : Int
+    , cellSize : Int
+    , borderSize : Int
+    , colors : List Color
+    }
+
+
+main : Program Never
+main =
+    let
+        purple =
+            Color.rgb 156 39 176
+
+        blue =
+            Color.rgb 33 150 243
+
+        teal =
+            Color.rgb 0 150 136
+
+        red =
+            Color.rgb 244 67 54
+
+        yellow =
+            Color.rgb 255 235 59
+
+        config =
+            { rows = 16
+            , cols = 16
+            , cellSize = 24
+            , borderSize = 2
+            , colors = [ purple, blue, teal, red, yellow ]
+            }
+
+        model =
+            emptyModel
+
+        startupCmd =
+            Random.generate NewBoard (init config)
+    in
+        Html.program
+            { init = ( model, startupCmd )
+            , update = update
+            , view = view
+            , subscriptions = always Sub.none
+            }
 
 
 type alias Model =
@@ -16,61 +69,77 @@ type alias Model =
     }
 
 
+emptyModel : Model
+emptyModel =
+    { board = Board.init 0 0 0 0
+    , cells = []
+    }
+
+
 type Msg
-    = NoOp
+    = NewBoard Model
 
 
-init : Int -> Int -> Int -> Int -> Model
-init rows cols cellSize borderSize =
+init : Config -> Random.Generator Model
+init config =
     let
         board =
-            Board.init rows cols cellSize borderSize
+            Board.init config.rows config.cols config.cellSize config.borderSize
 
-        cells =
-            List.map (\id -> Cell.init board id "#00aaff") [0..(cols * rows - 1)]
-    in
-        { board = board
-        , cells = cells
-        }
+        numCells =
+            config.cols * config.rows
 
+        ids =
+            [0..numCells - 1]
 
-main : Program Never
-main =
-    let
-        cols =
-            32
+        colorsGen =
+            Random.list numCells (randomColor config.colors)
 
-        rows =
-            32
+        cellsGen =
+            Random.map (List.map2 (Cell.init board) ids) colorsGen
 
-        cellSize =
-            16
-
-        borderSize =
-            2
-
-        model =
-            init cols rows cellSize borderSize
-    in
-        Html.program
-            { init = ( model, Cmd.none )
-            , update = update
-            , view = view (Cell.view model.board)
-            , subscriptions = always Sub.none
+        model cells =
+            { board = board
+            , cells = cells
             }
+    in
+        Random.map model cellsGen
+
+
+randomColor : List Color -> Random.Generator Color
+randomColor colors =
+    let
+        arr =
+            Array.fromList colors
+
+        gen =
+            Random.Array.sample arr
+
+        unwrap c =
+            case c of
+                Just color ->
+                    color
+
+                --should never happen. using black to make it stand out
+                Nothing ->
+                    Color.black
+    in
+        Random.map unwrap gen
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    model ! []
+    case msg of
+        NewBoard newModel ->
+            newModel ! []
 
 
-view : (Cell.Model -> Svg Msg) -> Model -> Html Msg
-view cellView model =
+view : Model -> Html Msg
+view model =
     div []
         [ Svg.svg
             [ width (toString model.board.width)
             , height (toString model.board.height)
             ]
-            (List.map cellView model.cells)
+            (List.map (Cell.view model.board) model.cells)
         ]
